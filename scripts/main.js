bca09f7f0c41afb205756230414813fdc84a56f8
Lukasz Serich <fargos990@gmail.com>
2022-10-09 20:37:33 +0200

    index gotowy :)

--- scripts/main.js
+++ scripts/main.js
@@ -1,44 +1,65 @@
-const apiKey = 'YTY2MzAyN2EtMjNlMi00OTFmLTk4NGItZTMxY2Y4MGQzMGVm'
-const images = document.getElementsByClassName("container__musicHolder__content__holder__frame__image");
+const API_KEY = '92b82c4c86b97fc11d5eea3c5f3e51fe'
+const LINK = 'https://ws.audioscrobbler.com/';
+
+const holders = document.getElementsByClassName("container__musicHolder__content__holder");
 
 let dataTab = [];
+let searchQuery = 'painted dreams'
 
-const app = async() =>
+const app = ()=>
 {
-
-    const response = await fetch(`https://api.napster.com/v2.2/tracks/top?apikey=${apiKey}&limit=6`,
-    {
-        method:"GET"
-    })
-
-    const data = await response.json();
-    dataTab = await [...data.tracks];
-    console.log(dataTab)
-    getPhoto(dataTab);
 
 }
 
-const getPhoto = async(dataTab)=>
+const searchForMusic = (searchFor) =>
 {
-    let i = 0;
-    let photosW = []
-    dataTab.map(async(elem)=>
+    fetch(`${LINK}/2.0/?method=track.search&track=${searchFor}&api_key=${API_KEY}&format=json&limit=1`).then((response)=>
     {
-        const response = await fetch(`https://api.napster.com/v2.2/albums/${elem.albumId}/images?apikey=${apiKey}`,
+        response.json().then((data)=>
         {
-            method:"GET"
+            console.log(data.results.trackmatches.track[0]);
         })
-        const data = await response.json();
-        //images[i].src = data.images[Math.floor(Math.random() * data.images.length )];
-        console.log(data.images[2].url);
-        i++;
     })
+}
+
+const getTopCountryTracks =(countryName)=>
+{
     
-    
-    
+    fetch(`${LINK}/2.0/?method=geo.gettoptracks&country=${countryName}&api_key=${API_KEY}&format=json&limit=6`).then((response)=>
+    {
+        response.json().then((data)=>
+        {
+            console.log(data);
+            for(let i = 0; i < holders.length; i++)
+            {
+                holders[i].children[0].children[0].src = `https://picsum.photos/seed/${data.tracks.track[i].artist.name}/110`;
+                holders[i].children[1].children[0].innerHTML = data.tracks.track[i].name;
+                holders[i].children[2].innerHTML = data.tracks.track[i].artist.name;
+                
+            }
+        })
+    })
 }
 
 app();
+
+//searchForMusic(searchQuery);
+getTopCountryTracks('poland');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
