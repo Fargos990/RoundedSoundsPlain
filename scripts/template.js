d92fe7574912006817712856c1515f7897907174
Lukasz Serich <fargos990@gmail.com>
2022-10-22 15:21:29 +0200

    czekanie na api lyrics

--- scripts/template.js
+++ scripts/template.js
@@ -2,11 +2,17 @@ const holders = document.getElementsByClassName("container__musicHolder__content
 
 const app = () =>
 {
+    const a = document.createElement('a');
     const index = JSON.parse(localStorage.getItem('indexOfItem'))
     dataTab = [...JSON.parse(localStorage.getItem('topTracks'))];
-    console.log(holders[0].children[0].children[0])
+
+    a.innerHTML = 'Check Full Song';
+    a.href = dataTab[index].url;
+
     holders[0].children[0].children[0].src = `https://picsum.photos/seed/${dataTab[index].artist.name}/110`
     holders[0].children[1].children[0].innerHTML = dataTab[index].name;
     holders[0].children[2].innerHTML = dataTab[index].artist.name;
+    holders[0].appendChild(a);
+    
 }
 app();
